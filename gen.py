fed79576989bc289c6468033c40085faa2385699
Abraham240 <zildjianremo@gmail.com>
2017-04-18 21:46:02 -0500

    FIX segunda seleccion

--- gen.py
+++ gen.py
@@ -372,7 +372,7 @@ Para esto lo que hace es lo siguiente
 """
 def SegundaSeleccion(idVector):
 	acumulador=0
-	ordenados=porcentaje_z
+	ordenados=z_acumulado
 	ordenados.sort()
 	print ordenados[3],
 	print "<",
@@ -394,8 +394,8 @@ def SegundaSeleccion(idVector):
 			acumulador=ordenados[0]
 		#ahora que se dentro de que rango esta, busco a que vector corresponde
 		#el valor de acumulador
-		for i in range(len(porcentaje_z)):
-			if porcentaje_z[i]==acumulador:
+		for i in range(len(z_acumulado)):
+			if z_acumulado[i]==acumulador:
 				veredicto[idVector]=i
 
 """
@@ -410,7 +410,7 @@ def Aleatorios():
 """
 
 def AcumulativoZ():
-	z_acumulado[0]=0
+	z_acumulado[0]=porcentaje_z[0]
 	for i in range(1,4):
 		z_acumulado[i]=z_acumulado[i-1]+porcentaje_z[i]
 
